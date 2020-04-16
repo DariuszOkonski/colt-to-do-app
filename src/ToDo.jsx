db5a37e8827945d0d633f4ca00a261b288180dc3
Dariusz Okonski <darek200180@gmail.com>
2020-04-16 21:41:13 +0100

    add methods toggleForm, handleChange, handleUpdata

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -3,22 +3,70 @@ import React, { Component } from 'react';
 class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isEditing: false,
+      task: this.props.task
+    }
 
     this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleRemove() {
     this.props.removeToDo(this.props.id);
   }
 
+  toggleForm() {
+    this.setState({
+      isEditing: !this.state.isEditing
+    })
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  handleUpdate(evt) {
+    evt.preventDefault();
+    this.props.updateToDo(this.props.id, this.state.task);
+
+    this.setState({
+      isEditing: false,
+    });
+  }
+
   render() {
-    return (
-      <li>
-        <button>Edit</button>
-        <button onClick={this.handleRemove}>X</button>
-        <div>{this.props.task}</div>
-      </li>
-    );
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <div>
+          <form onSubmit={this.handleUpdate}>
+            <input
+              type="text"
+              value={this.state.task}
+              name="task"
+              onChange={this.handleChange}
+            />
+            <button>Save</button>
+          </form>
+        </div>
+      )
+    } else {
+      result = (
+        <li>
+          <button onClick={this.toggleForm}>Edit</button>
+          <button onClick={this.handleRemove}>X</button>
+          <div>{this.props.task}</div>
+        </li>
+      )
+    };
+
+
+    return result;
   }
 }
 
