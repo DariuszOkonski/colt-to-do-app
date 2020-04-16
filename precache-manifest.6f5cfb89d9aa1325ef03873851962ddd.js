3a621b5d6962c6d9c4dcec3dc09ea48a4f4644d4
Dariusz Okonski <darek200180@gmail.com>
2020-04-16 20:15:37 +0100

    Updates

---
+++ precache-manifest.6f5cfb89d9aa1325ef03873851962ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d91782475b9bf387471e8463a2da1db",
+    "url": "/colt-to-do-app/index.html"
+  },
+  {
+    "revision": "41d1356afff5caea79ab",
+    "url": "/colt-to-do-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2584ce5972925a319ba3",
+    "url": "/colt-to-do-app/static/js/2.189ed78a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colt-to-do-app/static/js/2.189ed78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d1356afff5caea79ab",
+    "url": "/colt-to-do-app/static/js/main.9b7cc710.chunk.js"
+  },
+  {
+    "revision": "ca50e396e40185590c75",
+    "url": "/colt-to-do-app/static/js/runtime-main.eeb0a6f7.js"
+  }
+]);
