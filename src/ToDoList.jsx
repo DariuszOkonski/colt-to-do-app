44db6131e000ffde909980f14108150ecfa1084f
Dariusz Okonski <darek200180@gmail.com>
2020-04-16 21:05:37 +0100

    remove static object from todos array and add remove method

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -6,12 +6,10 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        { task: "Walk The Fish" },
-        { task: "Groom Chickens" }
-      ]
+      todos: []
     }
     this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
   }
 
   create(NewTodo) {
@@ -20,9 +18,20 @@ class TodoList extends Component {
     })
   }
 
+  remove(id) {
+    this.setState({
+      todos: this.state.todos.filter(t => t.id !== id)
+    });
+  }
+
   render() {
     const todos = this.state.todos.map(todo => {
-      return <Todo task={todo.task} />
+      return <Todo
+        key={todo.id}
+        task={todo.task}
+        id={todo.id}
+        removeToDo={this.remove}
+      />
     })
 
     return (
