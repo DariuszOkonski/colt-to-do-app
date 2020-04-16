55c4aecc72ec8a314a48c77e63091f6d4ddbed57
Dariusz Okonski <darek200180@gmail.com>
2020-04-16 21:05:01 +0100

    add remov item logic

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+
+  handleRemove() {
+    this.props.removeToDo(this.props.id);
+  }
+
   render() {
     return (
       <li>
         <button>Edit</button>
-        <button>X</button>
-        <li>{this.props.task}</li>
+        <button onClick={this.handleRemove}>X</button>
+        <div>{this.props.task}</div>
       </li>
     );
   }
